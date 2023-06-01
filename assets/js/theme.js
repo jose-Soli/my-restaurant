d8b1f04b10774ebd4ac2cde497324f446184ec2b
jose soli <joseestudios456@gmail.com>
2023-06-01 18:40:39 -0400

    Second Commit

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -5,3 +5,33 @@
  */
 
 "use strict";
+
+const $html = /* {nodeElemet} */ document.documentElement;
+const $isDark = /*bolean*/ window.matchMedia(
+    "(prefers-color-scheme: dark)"
+).matches;
+
+if (sessionStorage.getItem("theme")) {
+    $html.dataset.theme = sessionStorage.getItem("theme");
+} else {
+    $html.dataset.theme = $isDark ? "dark" : "light";
+}
+
+let isPressed = false;
+
+const changeTheme = function () {
+    isPressed = isPressed ? false : true;
+
+    this.setAttribute("aria-presse", isPressed);
+
+    $html.setAttribute(
+        "data-theme",
+        $html.dataset.theme == "light" ? "dark" : "light"
+    );
+    sessionStorage.setItem("theme", $html.dataset.theme);
+};
+
+window.addEventListener("load", function () {
+    const $themeBtn = this.document.querySelector("[data-theme-btn]");
+    $themeBtn.addEventListener("click", changeTheme);
+});
